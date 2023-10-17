06fc31ad6945bbdc823b13ba0b5ca9febf908f70
Unjustifiable1 <cupidojustin7@gmail.com>
2023-10-17 14:42:06 +0200

    js. set variables. dom selectors. dom interactions. started coding all these

--- mainscript.js
+++ mainscript.js
@@ -1,3 +1,41 @@
+// SET VARIABLES
+
+const DEFAULT_NUMBER_ONE = 0;
+const DEFAULT_OPERATOR = "+";
+const DEFAULT_NUMBER_TWO = 0;
+const DEFAULT_SELECTION = "numberOne"
+
+let currentNumberOne = DEFAULT_NUMBER_ONE;
+let currentOperator = DEFAULT_OPERATOR;
+let currentNumberTwo = DEFAULT_NUMBER_TWO;
+let currentSelection = DEFAULT_SELECTION;
+
+const setCurrentNumberOne = (newOne) => currentNumberOne = newOne;
+const setCurrentOperator = (newOperator) => currentOperator = newOperator;
+const setCurrentNumberTwo = (newTwo) => currentNumberTwo = newTwo;
+const setCurrentSelection = (newSelection) => currentSelection = newSelection;
+
+
+// DOM SELECTORS
+
+const numberOneBtn = document.getElementById('numberOne');
+const operator = document.getElementById('operator');
+const numberTwoBtn = document.getElementById('numberTwo');
+const calcAnswer = document.getElementById('calcAnswer');
+const equalsBtn = document.getElementById('equals');
+const clearBtn = document.getElementById('clear');
+const calcOperatorBtn = document.querySelectorAll('.calcOperator');
+
+
+// DOM INTERACTIONS
+
+numberOneBtn.onclick = () => setCurrentSelection('numberOne');
+numberTwoBtn.onclick = () => setCurrentSelection('numberTwo');
+clearBtn.onclick = () => clearCalc();
+
+
+// CALCULATOR FUNCTION
+
 function Calculator() {
 
     this.methods = {
@@ -30,3 +68,10 @@ function Calculator() {
 let calc = new Calculator;
 
 console.log(calc.operate("10 + 7"));
+
+
+// CALCULATOR FUNCTIONALITY
+
+const clearCalc = () => {
+
+};
